9d3efc3966ced81e2092c918599d381a900c26c9
Kristina Ahn <kristinaahn@Kristinas-iMac.local>
2023-04-17 09:56:03 -0700

    updated schema and seeds

--- seeds.sql
+++ seeds.sql
@@ -6,14 +6,14 @@ VALUES ("Engineering"),
 
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ("Salary Lead", "10000", 001),
-       ("Salesperson", "80000", 001),
-       ("Lead Engineer", "10000", 002),
-       ("Software Engineer","120000", 002),
-       ("Account Manager","160000", 003),
-		("Accountant", "125000", 003),
-       ("Legal Team Lead","250000", 004),
-       ("Lawyer", "190000", 004);
+VALUES ("Salary Lead", "10000", 4),
+       ("Salesperson", "80000", 4),
+       ("Lead Engineer", "10000", 1),
+       ("Software Engineer","120000", 1),
+       ("Account Manager","160000", 2),
+		("Accountant", "125000", 2),
+       ("Legal Team Lead","250000", 3),
+       ("Lawyer", "190000", 3);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
@@ -25,5 +25,4 @@ VALUES ("John", "Doe", 001, NULL),
 		("Malia", "Brown", 003, 005),
        ("Sarah","Lourd", 004, NULL),
        ("Tom", "Allen", 004, 007);
-
-
+       
